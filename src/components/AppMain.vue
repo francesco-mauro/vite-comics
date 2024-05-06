9c92dd92471e9e79fc88435251b1a6db73a1c494
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-06 19:44:57 +0200

    add: scaffolding and various checks

---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<script>
+export default{
+
+}
+</script>
+
+<template>
+  <h2> HEADER </h2>
+</template>
+
+<style scoped lang="Scss">
+</style>
---
+++ src/components/IconsList.vue
@@ -0,0 +1,11 @@
+<script>
+export default{
+}
+</script>
+
+<template>
+  <h2> HEADER </h2>
+</template>
+
+<style scoped lang="Scss">
+</style>
